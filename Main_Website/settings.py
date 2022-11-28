7adf971b8e4602258cadfe00e2f620d61c72fe76
Xtron135 <xtron135@gmail.com>
2022-11-28 10:12:53 -0500

    Test setup admin page

--- Main_Website/settings.py
+++ Main_Website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1','http://xtron135.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+	'SimpleApp.apps.SimpleappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- SimpleApp/models.py
+++ SimpleApp/models.py
@@ -1,3 +1,22 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Soalan(models.Model):
+	ayat_soalan = models.CharField(max_length=200)
+	tarikh_soalan = models.DateTimeField('date published')
+	def __str__(self):
+		return self.ayat_soalan
+	def soalan_baru(self):
+		return self.tarikh_soalan >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Pilihan(models.Model):
+	soalan = models.ForeignKey(Soalan, on_delete=models.CASCADE)
+	ayat_pilihan = models.CharField(max_length=200)
+	undian = models.IntegerField(default=0)
+	def __str__(self):
+		return self.ayat_pilihan
