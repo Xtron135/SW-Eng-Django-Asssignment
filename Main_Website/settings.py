96947c70a0e64ab531f7833b1718dad7c00a0bc8
Xtron135 <xtron135@gmail.com>
2022-11-28 09:10:56 -0500

    Update sikit

--- Main_Website/settings.py
+++ Main_Website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cg=bkmes@@xq0mow98ua($kv^yi)+^)*x(&y_^8xw-#xl!#3v%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','http://xtron135.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kuala_Lumpur'
 
 USE_I18N = True
 
